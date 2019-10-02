bf6e0dba48eb9a01ab48212ebcf724a08293fe06
Soloman Garcia <soloman.garcia@gmail.com>
2019-10-02 08:52:31 -0600

    created Spotify and OMDB functions, some debugging

--- liri.js
+++ liri.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
+var axios = require("axios");
 var keys = require("./keys.js");
 var fs = require("fs");
+var request = require("request");
+var Spotify = require("node-spotify-api");
 
 var spotify = new Spotify(keys.spotify);
+var omdbKey = keys.omdb.api_key;
 
 var command = process.argv[2];
 var secondCommand = process.argv[3];
@@ -16,21 +20,72 @@ switch (command) {
         if (secondCommand) {
             spotifyThisSong(secondCommand)
         } else {
-            spotifyThisSong("The sign");
+            spotifyThisSong("The Sign");
         }
         break;
 
     case "movie-this":
         if (secondCommand) {
-            ombd (secondCommand);
+            omdb(secondCommand);
         } else {
-            ombd("Mr. Nobody");
+            omdb("Mr. Nobody");
         }
         break;
-    
-        case "do-what-it-says":
-            doThing();
-            break;
-            default:
-                console.log("Try again")
+
+    case "do-what-it-says":
+        doThing();
+        break;
+    default:
+        console.log("Try again")
 };
+
+function omdb(movie) {
+    var omdbURL = "http://www.omdbapi.com/?t=" + movie + "&apikey=" + omdbKey + "&plot=short&tomatoes=true";
+
+    request(omdbURL, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var body = JSON.parse(body);
+
+            console.log("Title: " + body.Title);
+            console.log("Release Year: " + body.Year);
+            console.log("IMdB Rating: " + body.imdbRating);
+            console.log("Country: " + body.Country);
+            console.log("Language: " + body.Language);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+            console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+           
+
+        } else {
+            console.log("Error occurred.")
+        }
+        if (movie === "Mr. Nobody") {
+            console.log("-----------------------");
+            console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+            console.log("It's on Netflix!");
+
+        }
+    });
+
+}
+
+function spotifyThisSong(song) {
+    spotify.search({ type: 'track', query: song, limit: 1 }, function (error, data) {
+        if (!error) {
+            for (var i = 0; i < data.tracks.items.length; i++) {
+                var songData = data.tracks.items[i];
+                //artist
+                console.log("Artist: " + songData.artists[0].name);
+                //song name
+                console.log("Song: " + songData.name);
+                //spotify preview link
+                console.log("Preview URL: " + songData.preview_url);
+                //album name
+                console.log("Album: " + songData.album.name);
+                console.log("-----------------------");
+            }
+        } else {
+            console.log('Error occurred.');
+        }
+    });
+}
